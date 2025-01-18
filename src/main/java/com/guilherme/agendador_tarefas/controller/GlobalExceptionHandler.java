ac0061b3a6089625249976cc8fd1a01c9ba776c8
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-01-18 20:01:53 -0300

    adicionado handler, swagger e dockerfile

---
+++ src/main/java/com/guilherme/agendador_tarefas/controller/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.guilherme.agendador_tarefas.controller;
+
+import com.guilherme.agendador_tarefas.infrastructure.exceptions.ConflictException;
+import com.guilherme.agendador_tarefas.infrastructure.exceptions.ResourceNotFoundException;
+import com.guilherme.agendador_tarefas.infrastructure.exceptions.UnauthorizedException;
+import org.springframework.boot.context.config.ConfigDataException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConfigDataException.class)
+    public ResponseEntity<String> handleConflictException(ConflictException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
